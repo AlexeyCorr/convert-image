4151dccbfa796e9c7adde7dbf1855c9028599452
AlexeyCorr <alexeycorr@gmail.com>
2023-11-24 18:30:51 +0500

    Заменяет пакет для сжатия картинок на sqoosh

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,43 @@
 const { src, dest, series } = require('gulp')
 const del = require('del')
 const svgmin = require('gulp-svgmin')
-const imagemin = require('gulp-imagemin')
-const webp = require('gulp-webp')
-const avif = require('gulp-avif')
+const squoosh = require('gulp-libsquoosh')
 
-const SOURCE = 'src/image/**/*.{png,jpg}'
-const DESTINATION = 'dist/image'
+const SOURCE = './src/image/**/*.{png,jpg}'
+const DESTINATION = './dist/image'
 
 const svgMin = () => {
-  return src('src/svg/**/*.svg')
+  return src('./src/svg/**/*.svg')
     .pipe(svgmin())
-    .pipe(dest('dist/svg'))
+    .pipe(dest('./dist/svg'))
 }
 
 const imageMin = () => {
   return src(SOURCE)
-    .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 80, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
-      imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
-        ]
-      })
-    ]))
+    .pipe(squoosh())
     .pipe(dest(DESTINATION))
 }
 
 const toWebp = () => {
-  return src(`${DESTINATION}/**/*.{png,jpg}`)
-    .pipe(webp())
+  return src(SOURCE)
+    .pipe(
+      squoosh({
+        webp: {}
+      })
+    )
     .pipe(dest(DESTINATION))
 }
 
 const toAvif = () => {
-  return src(`${DESTINATION}/**/*.{png,jpg}`)
-    .pipe(avif())
-    .pipe(dest(DESTINATION))
+  return src(SOURCE)
+  .pipe(
+    squoosh({
+      avif: {}
+    })
+  )
+  .pipe(dest(DESTINATION))
 }
 
-const clean = () => del('dist')
+const clean = () => del('./dist')
 
 exports.minify = series(clean, svgMin, imageMin, toWebp, toAvif)
